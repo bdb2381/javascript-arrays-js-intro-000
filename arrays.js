cff5aad92273a2bb0fb4737df81eb683097c7b9e
Bradley Beggs <brad.beggs@gmail.com>
2020-06-10 02:23:49 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,19 +1,3 @@
-var ingredient1 = "bread"
-var ingredient2 = "mild cheese"
-var ingredient3 = "sharp cheese"
-var ingredient4 = "butter"
-var ingredient5 = "tomato"
-var ingredient6 = "garlic"
-
-/*
-Both functions take two parameters, an array and an element to add to the beginning of the array,
- and both functions should add the element to the beginning of the array and then return the whole array.
-  The destructive function, destructivelyAddElementToBeginningOfArray, should alter the original array
-  that's passed in; addElementToBeginningOfArray, on the other hand, should return a new array and not
-   modify the original.
-   */
-//var chocolateBars = [ "snickers", "hundred grand", "kitkat",  "skittles" ]; //yummy array of bars
-//var newBar = "Tea Bar"    //new type of bar. very cutting edge
 
 
 function addElementToBeginningOfArray(chocolateBars, newBar){   //return the new Bar but keep the old bars in a seperate list as well
