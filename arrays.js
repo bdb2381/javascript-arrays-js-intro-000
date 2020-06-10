6d87d73e50e2438a0a2561cb10617f8b906d9c48
Bradley Beggs <brad.beggs@gmail.com>
2020-06-10 01:56:55 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -9,7 +9,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", and "skittles"]
 /*
 Both functions take two parameters, an array and an element to add to the beginning of the array,
  and both functions should add the element to the beginning of the array and then return the whole array.
-  The destructive function, destructivelyAddElementToBeginningOfArray, should alter the original array 
+  The destructive function, destructivelyAddElementToBeginningOfArray, should alter the original array
   that's passed in; addElementToBeginningOfArray, on the other hand, should return a new array and not
    modify the original.
    */
